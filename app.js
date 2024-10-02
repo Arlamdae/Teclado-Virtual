526a632144945e06ebdaeea7f89f8141fae7d742
Arlamdae <natanielpenedo@gmail.com>
2024-10-02 19:50:26 -0300

    Major CSS and most JS

---
+++ app.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.btn')
+const textarea = document.querySelector('textarea')
+
+
+const delete_btn = document.querySelector('.delete')
+const shift_btn = document.querySelector('.shift')
+const space_btn = document.querySelector('.space')
+
+let chars = []
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        textarea.value += btn.innerText
+        chars = textarea.value.split('')
+        console.log(chars);
+    })
+})
+
+delete_btn.addEventListener('click', () => {
+    chars.pop()
+    textarea.value = chars.join ('')
+
+})
